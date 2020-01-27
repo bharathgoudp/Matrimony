7e23d81353b125032f1f87165ed173606d375c5d
Akshay406 <akshaykumarle6@gmail.com>
2020-01-27 17:02:00 +0530

    NewUpdate create djangoproject

---
+++ matrimony/matrimony/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mantrimony project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mantrimony.settings')
+
+application = get_asgi_application()
