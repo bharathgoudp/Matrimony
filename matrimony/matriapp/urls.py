b84088521fb53930fd621ffc7cebd636e4552c79
bharathgoudp <bharathgoudp01@gmail.com>
2020-01-28 12:21:04 +0530

    views and urls modified

--- matrimony/matriapp/urls.py
+++ matrimony/matriapp/urls.py
@@ -1,5 +1,15 @@
 from django.urls import path,include
 from matriapp import views
 urlpatterns = [
-     path('',views.index),
+     path('',views.index,name='index'),
+     path('myprofile/',views.myprofile,name='myprofile'),
+     path('myhome/',views.myhome,name='myhome'),
+     path('selfprofile/',views.selfprofile,name='selfprofile'), 
+     path('photos/',views.photos,name='photos'), 
+     path('profiles/',views.profiles,name='profiles'), 
+     path('search/',views.search,name='search'), 
+     path('step1/',views.step1,name='step1'), 
+     path('step2/',views.step2,name='step2'), 
+     path('step3/',views.step3,name='step3'),      
+     path('step4/',views.step4,name='step4'),
 ]
--- matrimony/matriapp/views.py
+++ matrimony/matriapp/views.py
@@ -4,3 +4,32 @@ from django.shortcuts import render
 def index(request):
     return render(request,'index.html')
 
+def myprofile(request):
+    return render(request,'my profile.html')
+
+def myhome(request):
+    return render(request,'myhome.html')
+
+def selfprofile(request):
+    return render(request,'myprofile.html') 
+
+def photos(request):
+    return render(request,'photos.html')
+
+def profiles(request):
+    return render(request,'profiles.html')
+
+def search(request):
+    return render(request,'search.html')
+
+def step1(request):
+    return render(request,'step1.html')    
+
+def step2(request):
+    return render(request,'step2.html')  
+
+def step3(request):
+    return render(request,'step3.html')  
+
+def step4(request):
+    return render(request,'step4.html')              
